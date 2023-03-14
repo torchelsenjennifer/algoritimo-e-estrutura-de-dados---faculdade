20526a8f35850dfabb67561ca10655ffee8279d5
Jennifer <jennifertorchelsen@gmail.com>
2023-03-14 19:02:13 -0300

    Atividades iniciais

---
+++ exercicioUm.py
@@ -0,0 +1,23 @@
+
+precos = []
+categoria = []
+while(True):
+    descricao = input("Descricao do Produto: ")
+    categoria.append(descricao)
+    if(descricao == 'Fim'):
+        break
+    valores = int(input("Informe o Preço do Produto: "))
+    precos.append(valores)
+    
+quantidade = len(precos)
+total = sum(precos)
+minimo = min(precos)
+media = total/2
+print(f"Quantidade de produtos: {quantidade}")
+print(f"Total em valores: {total}")
+print(f"Menor valor de produto: {minimo}")
+print(f"Media de valores dos produtos: {media}")
+
+for i in precos:
+    print(i, end = ' ')
+
