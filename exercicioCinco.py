7b5017ad848560a997ea95b8b631b5bddf700fc4
Jennifer <jennifertorchelsen@gmail.com>
2023-03-14 23:56:09 -0300

    adicionando lista

---
+++ exercicioCinco.py
@@ -0,0 +1,13 @@
+nome = []
+idade = []
+cidade = []
+
+while(True):
+     anos = int(input("Qual a sua idade: "))
+     idade.append(anos)
+     name = input("Insira o seu nome: ")
+     nome.append(name)
+     city = input("Qual a sua cidade: ")
+     cidade.append(city)
+
+     
